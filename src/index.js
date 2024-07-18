874e4457765c5e0df0a20201a58c4e6574d6a38f
rutu1518 <rutujamore1508@gmail.com>
2024-07-18 13:48:11 +0530

    news app created

--- src/index.js
+++ src/index.js
@@ -1,17 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+// import './index.css';
+import Home from "./view/Home/Home";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+
+// f9442c456b044999a971de656d39d483
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(<Home/>);
+
+
+
+
+
+
+
+
